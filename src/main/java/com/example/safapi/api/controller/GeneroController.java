26438dd5da158486a673c20ce7e4c400f0ea85fc
João Victor Senra <joaovictor.senra@estudante.ufjf.br>
2024-08-05 17:54:27 -0300

    Ajuste GeneroController

--- src/main/java/com/example/safapi/api/controller/GeneroController.java
+++ src/main/java/com/example/safapi/api/controller/GeneroController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/estudios")
+@RequestMapping("/api/v1/generos")
 @RequiredArgsConstructor
 @CrossOrigin
 
