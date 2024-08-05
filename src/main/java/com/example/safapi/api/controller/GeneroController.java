ea85460b27b9dea23d8d769e4db92dc528cc76e9
João Victor Senra <joaovictor.senra@estudante.ufjf.br>
2024-08-05 17:51:57 -0300

    Add GeneroController

---
+++ src/main/java/com/example/safapi/api/controller/GeneroController.java
@@ -0,0 +1,84 @@
+package com.example.safapi.api.controller;
+import com.example.safapi.api.dto.EstudioDTO;
+import com.example.safapi.api.dto.GeneroDTO;
+import com.example.safapi.model.entity.Genero;
+import com.example.safapi.exception.RegraNegocioException;
+import com.example.safapi.service.GeneroService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/estudios")
+@RequiredArgsConstructor
+@CrossOrigin
+
+public class GeneroController {
+    private final GeneroService service;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<Genero> genero = service.getGeneros();
+        return ResponseEntity.ok(genero.stream().map(GeneroDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Genero> ator = service.getGeneroById(id);
+        if (!ator.isPresent()) {
+            return new ResponseEntity("Gênero não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(ator.map(GeneroDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody EstudioDTO dto) {
+        try {
+            Genero genero = converter(dto);
+            genero = service.salvar(genero);
+            return new ResponseEntity(genero, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody EstudioDTO dto) {
+        if (!service.getGeneroById(id).isPresent()) {
+            return new ResponseEntity("Gênero não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Genero genero = converter(dto);
+            genero.setId(id);
+            service.salvar(genero);
+            return ResponseEntity.ok(genero);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Genero> genero = service.getGeneroById(id);
+        if (!genero.isPresent()) {
+            return new ResponseEntity("Gênero não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(genero.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    private Genero converter(EstudioDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Genero genero = modelMapper.map(dto, Genero.class);
+        return genero;
+    }
+}
